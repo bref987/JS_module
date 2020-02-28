e6b4041209c353a18d694032b730bf7ae7b3c335
Aleh Slutski <st.alehs@gmail.com>
2020-02-28 16:22:37 +0300

    refactor: respontype: arraybuffer

--- fs/rickandmorty.js
+++ fs/rickandmorty.js
@@ -1,18 +1,42 @@
 const fs = require('fs')
 const axios = require('axios')
 
-axios({
-  method: 'get',
-  url: 'https://rickandmortyapi.com/api/character/69',
-  responseType: 'json'
-})
-  .then(response => {
-    console.log(response.data);
-    fs.writeFileSync('./data/rickandmorty.txt',
-`Name : ${JSON.stringify(response.data.name)},
-Status : ${JSON.stringify(response.data.status)},
-Species : ${JSON.stringify(response.data.species)},
-Gender : ${JSON.stringify(response.data.gender)},
-Origin name : ${JSON.stringify(response.data.origin.name)},
-Location name : ${JSON.stringify(response.data.location.name)}`);
-});
+function getJson(url) {
+  return axios({
+    method: 'get',
+    url: url,
+    responseType: 'json'
+  })
+}
+
+function getJpeg(url) {
+  return axios({
+    method: 'get',
+    url: url,
+    responseType: 'arraybuffer'
+  })
+}
+
+function getResponseObject(res) {
+  const responseObject = {
+    Name: res.data.name,
+    Status: res.data.status,
+    Species: res.data.species,
+    Species: res.data.species,
+    Type: res.data.type,
+    "Origin name": res.data.origin.name,
+    "Location name": res.data.location.name
+  }
+  return responseObject;
+}
+
+axios.all([
+  getJson('https://rickandmortyapi.com/api/character/151'),
+  getJpeg('https://rickandmortyapi.com/api/character/avatar/151.jpeg')
+])
+  .then(axios.spread((respJson, respJpeg) => {
+
+    fs.writeFileSync('./data/rickandmorty.txt', JSON.stringify(getResponseObject(respJson)));
+    console.log(respJson.data);
+    fs.writeFileSync('./data/rickandmorty.jpeg', Buffer.from(respJpeg.data));
+}));
