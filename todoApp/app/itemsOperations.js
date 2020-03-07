b822eb75282a2defdf1de3540627821cb341907e
Aleh Slutski <st.alehs@gmail.com>
2020-03-07 18:12:28 +0300

    refactor: add existSyc for json

--- todoApp/app/itemsOperations.js
+++ todoApp/app/itemsOperations.js
@@ -1,52 +1,76 @@
 const fs = require('fs'); //add json if doesnt exist
 
 
-const notePath = './app/data/items.json'; //path attention to ./ or ../
+const itemsPath = './app/data/items.json'; //path attention to ./ or ../
+
+(() => {
+  try {
+    if (!fs.existsSync(itemsPath) || 
+        !Array.isArray(JSON.parse(fs.readFileSync(itemsPath)))) {
+      fs.writeFileSync(itemsPath, JSON.stringify([]));
+    } 
+  } catch {
+    fs.writeFileSync(itemsPath, JSON.stringify([]));
+  }
+})();
 
 function getItems() {
-  const content = Array.isArray(JSON.parse(fs.readFileSync(notePath, 'utf8'))) ?
-  JSON.parse(fs.readFileSync(notePath, 'utf8')) : [];
-  return content;
+  return JSON.parse(fs.readFileSync(itemsPath));
 }
 
 function addItem(title, body) { //add item with different title
   const addObj = {title: title, body: body},
   arrayJson    = getItems();
 
-  arrayJson.some(obj => obj.title === title) ? //or if else !!!
-  console.log(`Title "${title}" already exists`) :
-  (
-    arrayJson.push(addObj),
-    fs.writeFileSync(notePath, JSON.stringify(arrayJson, null, '\t')),
-    console.log(`Item "${title}" have been successfully added`)
-  );
+  if (!arrayJson.some(obj => obj.title === title)) {
+
+    arrayJson.push(addObj);
+    fs.writeFileSync(itemsPath, JSON.stringify(arrayJson, null, '\t'));
+    console.log(`Item "${title}" have been successfully added`);
+
+  } else {
+    console.log(`Title "${title}" already exists`);
+  }
 }
 
 function removeItem(title) { //remove existing item by his title; addItem logic possible
   const initialArrayJsonLength = getItems().length,
-  arrayJson = getItems().filter(obj => obj.title !== title);
+                     arrayJson = getItems().filter(obj => obj.title !== title);
 
-  arrayJson.length === initialArrayJsonLength ?
-  console.log(`Title "${title}" doesn't exist`) :
-  (
-    fs.writeFileSync(notePath, JSON.stringify(arrayJson, null, '\t')),
-    console.log(`Item "${title}" have been successfully removed`)
-  );
+  if (arrayJson.length !== initialArrayJsonLength) {
+
+    fs.writeFileSync(itemsPath, JSON.stringify(arrayJson, null, '\t'));
+    console.log(`Item "${title}" have been successfully removed`);
+
+  } else {
+    console.log(`Title "${title}" doesn't exist`);
+  }
 }
 
 function readItem(title) { //read an item if it exists
   const arrayJson = getItems();
 
-  arrayJson.some(obj => obj.title === title) ? //everywher if else
-  arrayJson.filter(obj => obj.title === title).forEach(obj => console.log(obj)) :
-  console.log(`Item "${title}" doesn't exist`);
+  if (arrayJson.some(obj => obj.title === title)) {
+
+    arrayJson
+      .filter(obj => obj.title === title)
+      .forEach(obj => console.log(obj));
+
+  } else {
+    console.log(`Item "${title}" doesn't exist`);
+  }
 }
 
 function listItems() { //list of all items if it exists
   const arrayJson = getItems();
 
-  arrayJson.length > 0 ? //if else also
-  arrayJson.forEach(obj => console.log(obj)) : console.log("No any items registered");
+  if (arrayJson.length > 0 ) {
+
+    arrayJson.forEach(obj => console.log(obj));
+
+  } else {
+    console.log("No any items registered");
+  }
 }
 
 
